b1de36c9c069116bc6bcdd4b946ef620c8fa3d2f
mapsartin <mapsartin@gmail.com>
2022-05-04 14:53:20 -0400

    React_0405

---
+++ exercicios-web/React_App/exercicios/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import ReactDOM from 'react-dom'
+var ReactDOM = require('react-dom')
+
+const elemento = <h1>React 2</h1>
+ReactDOM.render(elemento, document.getElementById('root'))
+
+// function Welcome(props) {
+//     return <h1>Olá, {props.name}</h1>;
+//   }
+  
+//   const element = <Welcome name="Sara" />;
+//   ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+//   );
+
+// ReactDOM.render()
+
+
